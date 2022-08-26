6f7e8fd1e2dd6c0a88438a961e98f6ab4fa2e6a7
taskmastarh <taskmastarh@gmail.com>
2022-08-26 14:54:32 +0100

    Reapeated String solution

---
+++ src/WarmUpChallenges/RepeatedStrings/RepeatedStrings.java
@@ -0,0 +1,51 @@
+package WarmUpChallenges.RepeatedStrings;
+
+public class RepeatedStrings {
+
+    public static void main(String[] args) {
+        System.out.println(FasterSolution(10, "aba"));
+    }
+
+    public static int Solution(int n,String s){
+
+        int counts = 0;
+        int occurance = 0;
+        boolean done = false;
+        while(!done){
+            for(int i= 0; i<s.length();i++){
+                if(String.valueOf(s.charAt(i)).equals("a")){
+                    occurance++;
+                }
+                counts++;
+                if(n==counts){
+                    done=true;
+                    break;
+                }
+            }
+        }
+
+        return occurance;
+    }
+
+    public static int FasterSolution(int n,String s ) {
+        // Write your code here
+        long occurance = 0;
+        boolean done = false;
+        long ocurInString=0;
+        long charLeft = (long) n%s.length();
+        long mul = (long) n/s.length();
+        long remCount=0;
+
+        for(int i = 0; i<s.length();i++){
+            if(String.valueOf(s.charAt(i)).equals("a")){
+                ocurInString++;
+            }
+        }
+        for (int j=0;j<charLeft;j++){
+            if(String.valueOf(s.charAt(j)).equals("a")){
+                remCount++;
+            }
+        }
+        occurance=(ocurInString*mul)+remCount;
+    }
+}
