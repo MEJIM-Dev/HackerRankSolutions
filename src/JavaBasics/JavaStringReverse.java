e990c3070e8b88ed581090d324e4c60691d966cd
taskmastarh <taskmastarh@gmail.com>
2022-08-29 00:39:39 +0100

    Java Strings Reverse -palidrome-

---
+++ src/JavaBasics/JavaStringReverse.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B= "";
+        for (int i = A.length()-1; i >= 0; i--) {
+            B+=A.charAt(i);
+        }
+        if(A.compareTo(B)==0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
