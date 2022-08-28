8f095b821c0106255a4ed6b59e4c0c82a353ec83
taskmastarh <taskmastarh@gmail.com>
2022-08-28 01:30:23 +0100

    Java 1-d Arrays

---
+++ src/JavaBasics/Java1DArrays.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+import java.util.*;
+
+public class Java1DArrays {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int j = 0; j < a.length; j++) {
+            a[j]=scan.nextInt();
+        }
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
