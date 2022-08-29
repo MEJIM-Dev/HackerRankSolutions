1ab7608db997911b2913cef1653ef3d4858ab1db
taskmastarh <taskmastarh@gmail.com>
2022-08-30 00:26:35 +0100

    Java Regex

---
+++ src/JavaBasics/JavaRegex.java
@@ -0,0 +1,28 @@
+package JavaBasics;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class JavaRegex
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        int i=0;
+        while(testCases>i){
+            String pattern = in.nextLine();
+            //Write your code
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch(Exception e){
+                System.out.println("Invalid");
+            }
+
+            i++;
+        }
+    }
+}
+
+
+
